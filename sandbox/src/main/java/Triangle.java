17615298d94d0f834b0e70b82717c37055d67b37
dmitryslez <stillov@mail.ru>
2025-03-12 21:58:12 +0300

    Внедрил класс Record

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,24 +1,11 @@
-public class Triangle {
-    double a;
-    double b;
-    double c;
-
-    public Triangle(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    //public static void main(String[] args) {
-        //Triangle triangle = new Triangle(5, 5, 5);
-       // System.out.println("Perimeter of triangle with sides " + "a = " +  triangle.a + ", " + "b = " + triangle.b + ", " + "c = " + triangle.c + " = " + triangle.trianglePerimeter());
-       // System.out.println("Area of triangle with sides " + "a = " + triangle.a + ", " + "b = "  + triangle.b + ", " +  "c = " + triangle.c + " = " + triangle.triangleArea());}
+public record Triangle (double a, double b, double c) {
 
     public double triangleArea() {
-        return Math.sqrt((trianglePerimeter()/2.0) * ((trianglePerimeter()/2.0) - this.a) * ((trianglePerimeter()/2.0) - this.b) * ((trianglePerimeter()/2.0) - this.c));
+        return Math.sqrt((trianglePerimeter()/2.0) * ((trianglePerimeter()/2.0) - a) * ((trianglePerimeter()/2.0) - b) * ((trianglePerimeter()/2.0) - c));
     }
 
     public double trianglePerimeter() {
-        return this.a + this.b + this.c;
+        return a + b + c;
     }
 
 }
