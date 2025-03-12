0a52be849946295762e6536836b35e7695342d06
dmitryslez <stillov@mail.ru>
2025-03-12 23:13:36 +0300

    Доработал конструктор для задания 3

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,5 +1,14 @@
 public record Triangle (double a, double b, double c) {
 
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if (a + b < c || a + c < b || c + b < a) {
+            throw new IllegalArgumentException("Such a triangle does not exist");
+        }
+    }
+
     public double triangleArea() {
         return Math.sqrt((trianglePerimeter()/2.0) * ((trianglePerimeter()/2.0) - a) * ((trianglePerimeter()/2.0) - b) * ((trianglePerimeter()/2.0) - c));
     }
--- sandbox/src/test/java/TriangleTest.java
+++ sandbox/src/test/java/TriangleTest.java
@@ -4,15 +4,33 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
 
     @Test
-    void CalculatePerimeter(){
+    void CalculatePerimeter() {
         var testTriangle = new Triangle(5.0,5.0,5.0);
         double result = testTriangle.trianglePerimeter();
         Assertions.assertEquals(15.0, result);
     }
     @Test
-    void CalculatArea(){
+    void CalculatArea() {
         var testTriangle = new Triangle(5.0,5.0,5.0);
         double result = testTriangle.triangleArea();
         Assertions.assertEquals(10.825317547305483, result);
+    }
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-5, -5, -5);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
+    @Test
+    void cannotCreateTriangleWithInequalityViolation() {
+        try {
+            new Triangle(1,2,5);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
     }
 }
