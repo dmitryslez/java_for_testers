e7c54f20886e0dd7a40e062b460df212f05d816d
dmitryslez <stillov@mail.ru>
2025-03-16 22:53:41 +0300

    Выполнил шаги в рамках лекции 2.5

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -14,7 +14,7 @@ public record Triangle (double a, double b, double c) {
     public double triangleArea() {
         return Math.sqrt((trianglePerimeter()/2.0) * ((trianglePerimeter()/2.0) - a) * ((trianglePerimeter()/2.0) - b) * ((trianglePerimeter()/2.0) - c));
     }
-
+  
     public double trianglePerimeter() {
         return a + b + c;
     }
