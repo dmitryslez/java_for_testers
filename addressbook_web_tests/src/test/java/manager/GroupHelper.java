0e1d4a3bde298feebd5abc6e7f1c7556aff1b2e1
dmitryslez <stillov@mail.ru>
2025-04-02 18:20:40 +0300

    Выполнил шаги по лекции 4.3

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -15,7 +15,7 @@ public class GroupHelper extends HelperBase {
             click(By.linkText("groups"));
         }
     }
-    
+
     public void createGroup(GroupData group) {
         openGroupsPage();
         initGroupCreation();
@@ -27,7 +27,7 @@ public class GroupHelper extends HelperBase {
     public void removeGroup() {
         openGroupsPage();
         selectGroup();
-        removeSelectedGroup();
+        removeSelectedGroups();
         returnToGroupsPage();
     }
 
@@ -49,7 +49,7 @@ public class GroupHelper extends HelperBase {
     }
 
 
-    private void removeSelectedGroup() {
+    private void removeSelectedGroups() {
         click(By.name("delete"));
     }
 
@@ -84,5 +84,18 @@ public class GroupHelper extends HelperBase {
         openGroupsPage();
         return manager.driver.findElements(By.name("selected[]")).size();
 
+    }
+
+    public void removeAllGroups() {
+        openGroupsPage();
+        selectAllGroups();
+        removeSelectedGroups();
+    }
+
+    private void selectAllGroups() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes) {
+            checkbox.click();
+        }
     }
 }
--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -16,6 +16,15 @@ public class GroupRemovalTests extends TestBase {
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount - 1, newGroupCount);
     }
-    
+
+    @Test
+    void canRemoveAllGroupsAtOnce() {
+        if (app.groups().getCount() == 0) {
+            app.groups().createGroup(new GroupData("name", "group header", "group footer"));
+        }
+        app.groups().removeAllGroups();
+        Assertions.assertEquals(0, app.groups().getCount());
+    }
+
 }
 
