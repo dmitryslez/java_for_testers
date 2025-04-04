7385857214e898487e345274dd3571a976afe866
dmitryslez <stillov@mail.ru>
2025-04-04 18:01:53 +0300

    Выполнил шаги по лекции 4.6

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -4,23 +4,31 @@ import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "name", "group header", "group footer"));
         }
-        int groupCount = app.groups().getCount();
-        app.groups().removeGroup();
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount - 1, newGroupCount);
+        var oldGropus = app.groups().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldGropus.size());
+        app.groups().removeGroup(oldGropus.get(index));
+        var newGropus = app.groups().getList();
+        var expectedList = new ArrayList<>(oldGropus);
+        expectedList.remove(index);
+        Assertions.assertEquals(newGropus,expectedList);
     }
 
     @Test
     void canRemoveAllGroupsAtOnce() {
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "name", "group header", "group footer"));
         }
         app.groups().removeAllGroups();
         Assertions.assertEquals(0, app.groups().getCount());
